ae13a82913cbcbb75e7bab921482a2ac00cdf94d
AndrewAguirreP <seesharpprojects@hotmail.com>
2020-01-22 00:39:55 +0800

    Add project files.

---
+++ MVCWebApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCWebApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index() 
+        {
+            return View();
+        }
+
+        //https://localhost:44366/HelloWorld/Welcome?name=Andrew&numTimes=4
+
+        public string Welcome(string name, int numTimes = 1) 
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}. Nuntimes is: {numTimes}");
+        }
+
+        //https://localhost:44366/HelloWorld/Welcome/4?name=Andrew
+        public string Hello(string name, int ID = 1) 
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name} your ID: {ID}");
+        }
+    }
+}
